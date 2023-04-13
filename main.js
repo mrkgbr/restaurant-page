216bf96abaf4c9581b0fc287a20cdb3cbcb6d1e4
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-04-13 06:27:51 +0200

    Add content to home

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-alert("Testing"),console.log("Testing too"),document.querySelector("#content").textContent="Hello Restaurant!";
+(()=>{"use strict";(()=>{const e=function(e,t,n){const d=document.createElement(e);return d.classList.add(t),n.appendChild(d),d},t=e("div","home",document.getElementById("content"));e("div","header",t),e("div","main",t),e("div","footer",t)})()})();
