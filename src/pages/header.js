43a7daa459d01c75023a940eec8ed49bdf77b9ab
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-04-13 13:07:25 +0200

    Add style and change createEle function

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,7 +1,26 @@
 // header contains: logo, buttons (pages), opening hours
 const header = () => {
+  // this function creates html elements
+  const createEle = function (tag, classText, appendTo, idText) {
+    const element = document.createElement(tag);
+    element.classList.add(classText);
+    if (idText) {
+      element.setAttribute("id", idText);
+    }
+    appendTo.appendChild(element);
+    return element;
+  };
+  // adding elements
+  // logo
   const header = document.querySelector(".header");
-  header.textContent = "This is the header";
+  createEle("div", "logo", header, "").textContent = "Logo";
+  // buttons
+  const button = createEle("div", "buttons", header, "");
+  createEle("button", "button", button, "button-1").textContent = "Home";
+  createEle("button", "button", button, "button-2").textContent = "Menu";
+  createEle("button", "button", button, "button-3").textContent = "Contact";
+  // info
+  createEle("div", "info", header, "").textContent = "Info";
 };
 
 export default header;
