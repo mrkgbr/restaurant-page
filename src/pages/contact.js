554125f4d037f3999b869bcdb010464e55c91ced
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-04-14 10:13:36 +0200

    Add contact contetn and style

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,18 @@
+import { createNewElement } from "../scripts";
+
 // contact contains: whole contact page in main
 const contact = () => {
   const contact = document.querySelector(".main");
-  contact.textContent = "This is the main of contact";
+  contact.textContent = "";
+  const contactContainer = createNewElement("div", contact, "", "", [
+    "contact-container",
+  ]);
+  createNewElement("h2", contactContainer, "Place your order now!", "", "");
+  createNewElement("h3", contactContainer, "World wide fast delivery!", "", "");
+  createNewElement("p", contactContainer, "+98 7654 3210", "", "");
+  createNewElement("h2", contactContainer, "We are here", "", ["map-header"]);
+  const map = createNewElement("img", contactContainer, "", "", ["map"]);
+  map.setAttribute("src", "./images/map.jpg");
 };
 
 export default contact;
